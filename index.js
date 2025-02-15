2710684b9bcb908439f0976b5f2ef0a753740910
Tanish <tk798833@gmail.com>
2025-02-15 20:50:08 +0530

    changes

--- index.js
+++ index.js
@@ -24,3 +24,7 @@ app.use("/api/courses", courseRoutes); // ✅ Added course routes
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
+  });
